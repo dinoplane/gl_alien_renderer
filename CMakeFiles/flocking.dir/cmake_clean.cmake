b3cc6a8bb770cb7dc17c74ba70b78bb631492ff7
dinoplane <isacsac2017@gmail.com>
2023-09-03 11:31:33 -0700

    added glm and glad

---
+++ CMakeFiles/flocking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flocking.dir/flocking/src/main.cpp.o"
+  "CMakeFiles/flocking.dir/flocking/src/main.cpp.o.d"
+  "flocking/flocking"
+  "flocking/flocking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flocking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/flocking.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for flocking.
+# This may be replaced when dependencies are built.
